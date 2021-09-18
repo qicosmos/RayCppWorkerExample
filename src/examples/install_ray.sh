7e2be23e844aba716f3223a77085032cb5c77d73
qicosmos <qicosmos@163.com>
2021-09-18 16:24:09 +0800

    install ray

---
+++ src/examples/install_ray.sh
@@ -0,0 +1,7 @@
+wget https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-2.0.0.dev0-cp36-cp36m-manylinux2014_x86_64.whl
+wget https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray_cpp-2.0.0.dev0-cp36-cp36m-manylinux2014_x86_64.whl
+pip install --force-reinstall ray-2.0.0.dev0-cp36-cp36m-manylinux2014_x86_64.whl
+pip install --force-reinstall ray_cpp-2.0.0.dev0-cp36-cp36m-manylinux2014_x86_64.whl cpp --find-links .
+
+mkdir ray-template && ray cpp --generate-bazel-project-template-to ray-template
+cp -r ray-template/thirdparty ./
